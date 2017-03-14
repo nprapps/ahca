34dd18a283e35a079d669b0293a11c8b13746fa9
Tyler Fisher <tyler@tylerjfisher.com>
2017-03-14 16:57:18 -0400

    move to urban/rural codes, create output

--- analyze.py
+++ analyze.py
@@ -1,26 +1,64 @@
 import agate
 import leather
 
-number_type = agate.Number()
+ahca = agate.Table.from_csv('data/combined.csv')
 
-def calculate_percent_enrolled(row):
-	return row['Plan Selections'] / row['Population']
+trump = ahca.where(lambda r: r['trump_votepct'] > r['clinton_votepct'])
+clinton = ahca.where(lambda r: r['clinton_votepct'] > r['trump_votepct'])
 
-signups = agate.Table.from_csv('data/combined.csv')
-percent_enrolled = signups.compute([
-	('percent_enrolled', agate.Formula(number_type, calculate_percent_enrolled))
-])
+rural = ahca.where(lambda r: r['RUCC_2013'] > 7)
+small_towns = ahca.where(lambda r: 3 < r['RUCC_2013'] <= 7)
+metro = ahca.where(lambda r: r['RUCC_2013'] <= 3)
 
-trump = percent_enrolled.where(lambda r: r['trump_votepct'] > r['clinton_votepct'])
-clinton = percent_enrolled.where(lambda r: r['clinton_votepct'] > r['trump_votepct'])
 
-print(trump.aggregate(agate.Mean('percent_enrolled')))
-print(clinton.aggregate(agate.Mean('percent_enrolled')))
+include = [
+    'FIPS',
+    'ST',
+    'County',
+    "Dollar difference for 27 year old with $20,000 income",
+    "Dollar differencefor 27 year old with $30,000 income",
+    "Dollar difference for 27 year old with $40,000 income",
+    "Dollar difference for 27 year old with $50,000 income",
+    "Dollar difference for 40 year old with $20,000 income",
+    "Dollar difference for 40 year old with $30,000 income",
+    "Dollar difference for 40 year old with $40,000 income",
+    "Dollar difference for 40 year old with $50,000 income",
+    "Dollar difference for 60 year old with $20,000 income",
+    "Dollar difference for 60 year old with $30,000 income",
+    "Dollar difference for 60 year old with $40,000 income",
+    "Dollar difference for 60 year old with $50,000 income",
+    'RUCC_2013',
+    'trump_votepct',
+    'clinton_votepct'
+]
+print(include)
 
-outlier = percent_enrolled.order_by('percent_enrolled', reverse=True).rows[0]
+trump.select(include).to_csv('data/output/trump_counties.csv')
+clinton.select(include).to_csv('data/output/clinton_counties.csv')
+rural.select(include).to_csv('data/output/rural_counties.csv')
+small_towns.select(include).to_csv('data/output/town_counties.csv')
+metro.select(include).to_csv('data/output/metro_counties.csv')
 
-print(outlier['County Name'], outlier['State'], outlier['percent_enrolled'], outlier['Population'], outlier['Plan Selections'])
 
-percent_enrolled.scatterplot('trump_votepct', 'percent_enrolled', 'trump_vs_enrollment.svg')
-percent_enrolled.scatterplot('Median Income', 'percent_enrolled', 'income_vs_enrollment.svg')
-percent_enrolled.scatterplot('Percent White', 'percent_enrolled', 'white_vs_enrollment.svg')
+ages = ['27', '40', '60']
+incomes = ['$20,000', '$40,000', '$50,000', '$75,000']
+
+for age in ages:
+    for income in incomes:
+        print(
+            '{0} with {1} \n'.format(age, income),
+            'Trump: ',
+            trump.aggregate(agate.Mean('Dollar difference for {0} year old with {1} income'.format(age, income))),
+            '\n',
+            'Clinton: ',
+            clinton.aggregate(agate.Mean('Dollar difference for {0} year old with {1} income'.format(age, income))),
+            '\n',
+            'Rural: ',
+            rural.aggregate(agate.Mean('Dollar difference for {0} year old with {1} income'.format(age, income))),
+            '\n',
+            'Small Towns: ',
+            small_towns.aggregate(agate.Mean('Dollar difference for {0} year old with {1} income'.format(age, income))),
+            '\n',
+            'Metro: ',
+            metro.aggregate(agate.Mean('Dollar difference for {0} year old with {1} income'.format(age, income)))
+        )
--- process.sh
+++ process.sh
@@ -4,8 +4,10 @@
 python clean.py
 python combine_census.py
 python election.py
+in2csv -f xls data/original/ruralurbancodes2013.xls | csvcut -c FIPS,RUCC_2013 > data/codes.csv
 
 # join
-csvjoin -c FIPS,FIPS,fips data/ahca.csv data/census.csv data/elex.csv > data/combined.csv
+csvjoin -c FIPS,FIPS,FIPS,fips data/ahca.csv data/census.csv data/codes.csv data/elex.csv > data/combined.csv
 
 # process
+python analyze.py
