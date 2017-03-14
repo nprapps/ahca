2774c1ca9834fb171aae19ac5b2f0d4873d34bde
Tyler Fisher <tyler@tylerjfisher.com>
2017-03-14 17:30:35 -0400

    spaces

--- combine_census.py
+++ combine_census.py
@@ -5,24 +5,24 @@ import os
 path = 'data/census'
 
 def process():
-	with open('data/census.csv', 'w') as writefile:
-		writer = csv.writer(writefile)
-		writer.writerow(['FIPS', 'Population', 'Median Income', 'Percent White'])
+    with open('data/census.csv', 'w') as writefile:
+        writer = csv.writer(writefile)
+        writer.writerow(['FIPS', 'Population', 'Median Income', 'Percent White'])
 
-		for filename in os.listdir('data/census'):
-			with open('{0}/{1}'.format(path, filename)) as f:
-				data = json.load(f)
-				for key, item in data.items():
-					row = create_row(key, item)
-					writer.writerow(row)
+        for filename in os.listdir('data/census'):
+            with open('{0}/{1}'.format(path, filename)) as f:
+                data = json.load(f)
+                for key, item in data.items():
+                    row = create_row(key, item)
+                    writer.writerow(row)
 
 def create_row(key, item):
-	return [
-		key,
-		item['census']['population'],
-		item['census']['median_income'],
-		item['census']['percent_white']
-	]
+    return [
+        key,
+        item['census']['population'],
+        item['census']['median_income'],
+        item['census']['percent_white']
+    ]
 
 if __name__ == '__main__':
-	process()
+    process()
