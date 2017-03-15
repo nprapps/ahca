eb7a3aa87c06f50ce13698a147b607dc920e92d0
Tyler Fisher <tyler@tylerjfisher.com>
2017-03-15 17:04:32 -0400

    adjacency testing, urban towns and tiny towns

--- analyze.py
+++ analyze.py
@@ -10,6 +10,13 @@ rural = ahca.where(lambda r: r['RUCC_2013'] > 7)
 small_towns = ahca.where(lambda r: 3 < r['RUCC_2013'] <= 7)
 metro = ahca.where(lambda r: r['RUCC_2013'] <= 3)
 
+urban_towns = ahca.where(lambda r: 3 < r['RUCC_2013'] <= 5)
+tiny_towns = ahca.where(lambda r: 5 < r['RUCC_2013'] <= 7)
+
+
+adjacent = ahca.where(lambda r: r['RUCC_2013'] == 4 or r['RUCC_2013'] == 6 or r['RUCC_2013'] == 8)
+not_adjacent = ahca.where(lambda r: r['RUCC_2013'] == 5 or r['RUCC_2013'] == 7 or r['RUCC_2013'] == 9)
+
 def print_breakdown():
     ages = ['27', '40', '60']
     incomes = ['$20,000', '$30,000', '$40,000', '$50,000', '$75,000']
@@ -31,8 +38,31 @@ def print_breakdown():
                 small_towns.aggregate(agate.Mean('Dollar difference for {0} year old with {1} income'.format(age, income))),
                 '\n',
                 'Metro: ',
-                metro.aggregate(agate.Mean('Dollar difference for {0} year old with {1} income'.format(age, income)))
+                metro.aggregate(agate.Mean('Dollar difference for {0} year old with {1} income'.format(age, income))),
+                '\n',
+                'Adjacent to metro: ',
+                adjacent.aggregate(agate.Mean('Dollar difference for {0} year old with {1} income'.format(age, income))),
+                '\n',
+                'Not adjacent to metro: ',
+                not_adjacent.aggregate(agate.Mean('Dollar difference for {0} year old with {1} income'.format(age, income)))
             )
+
+
+def charts():
+    column_names = ['group', 'difference']
+    column_types = [agate.Text(), agate.Number()]
+
+    rows = [
+        ('60 $20,000 not adjacent', not_adjacent.aggregate(agate.Mean('Dollar difference for 60 year old with $20,000 income'))),
+        ('60 $20,000 adjacent', adjacent.aggregate(agate.Mean('Dollar difference for 60 year old with $20,000 income'))),
+        ('60 $20,000 metro', metro.aggregate(agate.Mean('Dollar difference for 60 year old with $20,000 income')))
+    ]
+
+    charting_table = agate.Table(rows, column_names, column_types)
+
+    charting_table.bar_chart('group', 'difference', '60-20k-adjacent.svg')
+
+
 
 def write_csvs():
     include = [
@@ -62,6 +92,9 @@ def write_csvs():
     rural.select(include).to_csv('data/output/rural_counties.csv')
     small_towns.select(include).to_csv('data/output/town_counties.csv')
     metro.select(include).to_csv('data/output/metro_counties.csv')
+
+    urban_towns.select(include).to_csv('data/output/urban_town_counties.csv')
+    tiny_towns.select(include).to_csv('data/output/tiny_town_counties.csv')
 
 if __name__ == '__main__':
     print_breakdown()
