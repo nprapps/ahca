355a70999dafc060c00eb306c461e1904cbde5b2
Tyler Fisher <tyler@tylerjfisher.com>
2017-03-23 10:23:04 -0400

    cleanup

--- analyze.py
+++ analyze.py
@@ -8,8 +8,6 @@ specified_types = {
 
 ahca = agate.Table.from_csv('data/fixed_fips.csv', column_types=specified_types).where(lambda r: r['FIPS'] != '51515')
 
-# trump = ahca.where(lambda r: r['trump_votecount'] > r['clinton_votecount'])
-# clinton = ahca.where(lambda r: r['clinton_votecount'] > r['trump_votecount'])
 rural = ahca.where(lambda r: r['RUCC_2013'] > 7)
 small_towns = ahca.where(lambda r: 3 < r['RUCC_2013'] <= 7)
 metro = ahca.where(lambda r: r['RUCC_2013'] <= 3)
@@ -22,12 +20,6 @@ def print_breakdown():
         for income in incomes:
             print(
                 '{0} with {1} \n'.format(age, income),
-                # 'Trump:',
-                # trump.aggregate(agate.Mean('Dollar difference for {0} year old with ${1},000 income'.format(age, income))),
-                # '\n',
-                # 'Clinton:',
-                # clinton.aggregate(agate.Mean('Dollar difference for {0} year old with ${1},000 income'.format(age, income))),
-                # '\n',
                 'Rural:',
                 rural.aggregate(agate.Mean('Dollar difference for {0} year old with ${1},000 income'.format(age, income))),
                 '\n',
@@ -81,8 +73,6 @@ def write_csvs():
     ]
 
     ahca.select(include).to_csv('data/output/all.csv')
-    # trump.select(include).to_csv('data/output/trump_counties.csv')
-    # clinton.select(include).to_csv('data/output/clinton_counties.csv')
     rural.select(include).to_csv('data/output/rural_counties.csv')
     small_towns.select(include).to_csv('data/output/town_counties.csv')
     metro.select(include).to_csv('data/output/metro_counties.csv')
